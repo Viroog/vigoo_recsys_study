1b86760e49b39d5142f530088bdd13cd788cfe12
刘秋祥 <1506240092@qq.com>
2023-08-27 11:47:06 +0800

    完成了User Based OCCF，Hybrid模型以后再实现。下面实现贝叶斯个性化推荐算法

--- Memory-Based OCCF/model.py
+++ Memory-Based OCCF/model.py
@@ -26,7 +26,11 @@ class ItemCF:
         return list(sim_seires.iloc[:self.K].index)
 
     def get_Iu(self, user):
-        return self.u_i_dict[user]
+
+        if user in self.u_i_dict.keys():
+            return self.u_i_dict[user]
+        else:
+            return list()
 
     def train(self, data):
 
@@ -90,6 +94,97 @@ class ItemCF:
                     ranking_df.iloc[index, 1] = pred
 
                     ranking_df.sort_values(by='rating', ascending=False, inplace=True)
+
+                ranking[user] = ranking_df['item'].tolist()
+
+        return ranking, ground_truth
+
+
+class UserCF:
+    def __init__(self, user_nums, item_nums, K):
+        self.user_nums = user_nums
+        self.item_nums = item_nums
+        self.K = K
+
+        # 相似度矩阵
+        self.user_sim_mat = None
+        self.u_i_dict = {}
+        self.i_u_dict = {}
+
+    # 获得对物品j交互过的用户
+    def get_Uj(self, j):
+        if j in self.i_u_dict:
+            return self.i_u_dict[j]
+        else:
+            return list()
+
+    # 获得用户的K近邻
+    def get_Nu(self, user):
+        sim_series = self.user_sim_mat.iloc[user]
+        sim_series.sort_values(ascending=False, inplace=True)
+        sim_series = sim_series.iloc[1:]
+
+        return list(sim_series.iloc[:self.K].index)
+
+    def train(self, data):
+        # 交互矩阵
+        u_i_mat = pd.DataFrame(np.zeros((self.user_nums, self.item_nums)), index=np.arange(0, self.user_nums),
+                               columns=np.arange(0, self.item_nums))
+
+        for i in range(len(data)):
+            user, item, rating = data[i, :]
+            u_i_mat.iloc[user, item] = 1
+
+            if user not in self.u_i_dict.keys():
+                self.u_i_dict[user] = [item]
+            else:
+                self.u_i_dict[user].append(item)
+
+            if item not in self.i_u_dict.keys():
+                self.i_u_dict[item] = [user]
+            else:
+                self.i_u_dict[item].append(user)
+
+        user_sim_mat = 1 - pairwise_distances(u_i_mat.values, metric='jaccard')
+        self.user_sim_mat = pd.DataFrame(user_sim_mat, index=np.arange(0, self.user_nums),
+                                         columns=np.arange(0, self.user_nums))
+
+        # normalization
+        for index, row in self.user_sim_mat.iterrows():
+            # 最大值为1，即自身与自身的相似度
+            second_large = sorted(row, reverse=True)[1]
+            self.user_sim_mat.loc[index] = row / second_large
+
+    def test(self, data):
+        ranking, ground_truth = {}, {}
+        total_item_list = [i for i in range(self.item_nums)]
+
+        for i in range(len(data)):
+            user, item, rating = data[i, :]
+
+            if user not in ground_truth.keys():
+                ground_truth[user] = [item]
+            else:
+                ground_truth[user].append(item)
+
+            if user not in ranking.keys():
+                uninteract_item_list = list(set(total_item_list) - set(self.u_i_dict[user]))
+
+                ranking_df = pd.DataFrame(index=np.arange(0, len(uninteract_item_list)), columns=['item', 'rating'])
+
+                for index, j in enumerate(uninteract_item_list):
+                    Uj = self.get_Uj(j)
+                    Nu = self.get_Nu(user)
+
+                    W = list(set(Uj).intersection(set(Nu)))
+
+                    pred = 0
+                    for w in W:
+                        pred += self.user_sim_mat.iloc[w, user]
+
+                    ranking_df.iloc[index, :] = j, pred
+
+                ranking_df.sort_values(by='rating', ascending=False, inplace=True)
 
                 ranking[user] = ranking_df['item'].tolist()
 
--- Memory-Based OCCF/predict.py
+++ Memory-Based OCCF/predict.py
@@ -1,12 +1,14 @@
 from data import Data
-from model import ItemCF
+from model import ItemCF, UserCF
 from evaluation import Evaluation
 
 train_data, test_data = Data("../data/ml-100k/").get_data()
+print("load data finished...")
 user_nums = 943
 item_nums = 1682
 K = 50
 icf = ItemCF(user_nums, item_nums, K)
+print("start training ICF...")
 icf.train(train_data)
 ranking, ground_truth = icf.test(test_data)
 
@@ -14,5 +16,16 @@ ranking, ground_truth = icf.test(test_data)
 k = 5
 evaluation = Evaluation(ranking, ground_truth, k)
 print("Item Based OCCF:")
-print(f"Pre@{k}:{evaluation.precision()}")
-print(f"Recall@{k}:{evaluation.recall()}")
+print(f"Precision@{k}: {evaluation.precision()}")
+print(f"Recall@{k}: {evaluation.recall()}")
+
+
+ucf = UserCF(user_nums, item_nums, K)
+print("start training UCF...")
+ucf.train(train_data)
+ranking, ground_truth = ucf.test(test_data)
+
+evaluation = Evaluation(ranking, ground_truth, k)
+print("Uer Base OCCF:")
+print(f"Precision@{k}: {evaluation.precision()}")
+print(f"Recall@{k}: {evaluation.recall()}")
