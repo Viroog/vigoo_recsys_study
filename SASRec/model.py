61381913bebd95a2393a2493cf85636b72cf126c
刘秋祥 <1506240092@qq.com>
2023-10-16 11:53:53 +0800

    完成了模型的前向传播，下一步完成损失的计算和损失的反向传播

--- SASRec/model.py
+++ SASRec/model.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 
 import numpy as np
 
-
+# 这里实现两种模型: 1) 按照论文的模型实现
+#                2) 利用Pytorch中的TransformerEncoderLayer和TransformerEncoder构建
+# 两个模型的主要差距在于Dropout和Layer Norm的时机，前馈神经网络的架构也不太一样
 class SASRec(nn.Module):
-    def __init__(self, n, item_nums, d, dropout_rate):
+    def __init__(self, n, item_nums, d, dropout_rate, block_nums, head_nums):
         super(SASRec, self).__init__()
         # n为能够处理的序列最大长度
         self.n = n
@@ -13,10 +15,22 @@ class SASRec(nn.Module):
         # d为latent dimension
         self.d = d
         self.dropout_rate = dropout_rate
+        # self-attention+ffn为一个block
+        self.block_nums = block_nums
+        # 自注意力头数
+        self.head_nums = head_nums
+
+        # 下三角形的单位矩阵，并将数据放到gpu上
+        self.mask = torch.tril(torch.ones((self.n, self.n))).cuda()
 
         self.item_embedding = nn.Embedding(self.item_nums + 1, self.d)
         self.position_embedding = nn.Embedding(self.n, self.d)
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.dropout = nn.Dropout(p=self.dropout_rate)
+
+        # 里面带有一个self-attention和一个FFN
+        # 将batch_fist设置为True，才是(batch_size, seq, feature_dim)，要不然是(seq, batch_size, feature_dim)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.d, nhead=self.head_nums, dim_feedforward=self.n, dropout=self.dropout_rate, batch_first=True)
+        self.encoder = nn.TransformerEncoder(encoder_layer=self.encoder_layer, num_layers=self.block_nums)
 
         self.cuda()
 
@@ -32,13 +46,27 @@ class SASRec(nn.Module):
         position = np.tile(np.array(range(pos.shape[1])), reps=[pos.shape[0], 1])
         position = torch.LongTensor(position).cuda()
         # shape: (batch_size, n, d)
-        pos_embed = self.position_embedding(position)
+        position_embed = self.position_embedding(position)
 
         # shape: (batch_size, n, d)
-        input = seq_embed + pos_embed
+        input_embed = self.dropout(seq_embed + position_embed)
 
-        # 接下来经过self-attention
-        pass
+        # 要有一个mask，使得attention中过去的能影响未来，而不能未来影响过去
+        # 这里的输出对应模型最上方的向量
+        # shape: (batch_size, n, d)
+        output_embed = self.encoder(input_embed, self.mask)
+
+        # 正样本和负样本的embedding
+        pos_embed = self.item_embedding(pos)
+        neg_embed = self.item_embedding(neg)
+
+        # 预测公式：r_(i,t) = F_t * M_i
+        # 并对最后一维求和，即获得预测分数
+        # shape: (batch_size, n)
+        pos_pred = (pos_embed * output_embed).sum(axis=-1)
+        neg_pred = (neg_embed * output_embed).sum(axis=-1)
+
+        return pos_pred, neg_pred
 
     def test(self):
         pass
--- SASRec/train.py
+++ SASRec/train.py
@@ -3,18 +3,19 @@ import numpy as np
 
 from data import Data, Sampler
 from model import SASRec
+import torch.optim as optim
 
 # 整合参数，比较标准
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', default='./data/ml-1m.txt')
 parser.add_argument('--batch_size', default=128)
 parser.add_argument('--lr', default=0.001)
-parser.add_argument('--optimizer', default='Adam')
 parser.add_argument('--epochs', default=200)
 parser.add_argument('--n', default=200)
 parser.add_argument('--d', default=50)
 parser.add_argument('--block_nums', default=2)
 parser.add_argument('--dropout_rate', default=0.2)
+parser.add_argument('--head_nums', default=1)
 
 args = parser.parse_args()
 
@@ -25,7 +26,9 @@ user_train, user_valid, user_test, user_nums, item_nums = data.user_train, data.
 sampler = Sampler(user_train, user_nums, item_nums, args.batch_size, args.n)
 batch_nums = len(user_train) // args.batch_size
 
-sasrec = SASRec(args.n, item_nums, args.d, args.dropout_rate)
+sasrec = SASRec(args.n, item_nums, args.d, args.dropout_rate, args.block_nums, args.head_nums)
+
+adam = optim.Adam(sasrec.parameters(), lr=args.lr)
 
 sasrec.train()
 for epoch in range(args.epochs):
@@ -34,4 +37,5 @@ for epoch in range(args.epochs):
         # (batch_size, n)
         user, seq, pos, neg = np.array(user), np.array(seq), np.array(pos), np.array(neg)
 
-        sasrec.forward(seq, pos, neg)
+        pos_pred, neg_pred = sasrec(seq, pos, neg)
+
