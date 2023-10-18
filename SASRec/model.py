63c9e00c05d335808536c970ec5ae968a0b468d6
刘秋祥 <1506240092@qq.com>
2023-10-18 11:59:49 +0800

    完成了论文中的模型架构

--- SASRec/model.py
+++ SASRec/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+
 #
 # 这里实现两种模型: 1) 按照论文的模型实现
 #                2) 利用Pytorch中的TransformerEncoderLayer和TransformerEncoder构建
@@ -33,6 +34,7 @@ class SASRec(nn.Module):
         # 将batch_fist设置为True，才是(batch_size, seq, feature_dim)，要不然是(seq, batch_size, feature_dim)
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.d, nhead=self.head_nums, dim_feedforward=self.d,
                                                         dropout=self.dropout_rate, batch_first=True)
+        # 这里效果差了一点的原因可能是少经过了一层FFN
         self.encoder = nn.TransformerEncoder(encoder_layer=self.encoder_layer, num_layers=self.block_nums)
 
         self.cuda()
@@ -72,7 +74,6 @@ class SASRec(nn.Module):
         return pos_pred, neg_pred
 
     def predict(self, seq, item_idxs):
-
         seq, item_idxs = torch.LongTensor(seq).cuda(), torch.LongTensor(item_idxs).cuda()
 
         # shape: (1, n, d)
@@ -102,3 +103,76 @@ class SASRec(nn.Module):
         preds = item_embeds.matmul(last_output.unsqueeze(-1)).squeeze(-1)
 
         return preds
+
+
+# 源码使用的是一维卷积代替全连接网络
+class PointWiseFeedForward(nn.Module):
+    def __init__(self, hidden_nums, dropout_rate, used_conv=True):
+        super(PointWiseFeedForward, self).__init__()
+
+        self.hidden_nums = hidden_nums
+        self.dropout_rate = dropout_rate
+
+        # used_conv用来选择是否用卷积层来代替全连接层
+        if used_conv:
+            # 卷积核的大小由kernel_size*in_channel来决定
+            # 输入维度和输出维度不变
+            self.fc1 = nn.Conv1d(in_channels=self.hidden_nums, out_channels=self.hidden_nums, kernel_size=1)
+            self.fc2 = nn.Conv1d(in_channels=self.hidden_nums, out_channels=self.hidden_nums, kernel_size=1)
+        else:
+            self.fc1 = nn.Linear(self.hidden_nums, self.hidden_nums)
+            self.fc2 = nn.Linear(self.hidden_nums, self.hidden_nums)
+
+        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(self.dropout_rate)
+        self.dropout2 = nn.Dropout(self.dropout_rate)
+
+    def forward(self):
+        pass
+
+
+class SASRec2(nn.Module):
+    def __init__(self, n, item_nums, d, dropout_rate, block_nums, head_nums):
+        super(SASRec2, self).__init__()
+
+        self.n = n
+        self.item_nums = item_nums
+        self.d = d
+        self.dropout_rate = dropout_rate
+        self.block_nums = block_nums
+        self.head_nums = head_nums
+
+        self.item_embed = nn.Embedding(self.item_nums + 1, self.d, padding_idx=0)
+        self.position_embed = nn.Embedding(self.n, self.d)
+        self.embed_dropout = nn.Dropout(self.dropout_rate)
+
+        # self-attention之前的layer norm
+        self.Attention_normed = nn.ModuleList()
+        # dropout在MutiheadAttention中自带
+        self.Attention = nn.ModuleList()
+        # FFN之前的layer norm
+        self.FFN_normed = nn.ModuleList()
+        self.FFN = nn.ModuleList()
+
+        for _ in range(self.block_nums):
+            Attention_normed = nn.LayerNorm(self.d, eps=1e-8)
+            self.Attention_normed.append(Attention_normed)
+
+            Attention = nn.MultiheadAttention(embed_dim=self.d, num_heads=self.head_nums, dropout=self.dropout_rate,
+                                              batch_first=True)
+            self.Attention.append(Attention)
+
+            FFN_normed = nn.LayerNorm(self.d, eps=1e-8)
+            self.FFN_normed.append(FFN_normed)
+
+            FFN = PointWiseFeedForward(self.d, self.dropout_rate)
+            self.FFN.append(FFN)
+
+    def forward(self, seq, pos, neg):
+        seq, pos, neg = torch.LongTensor(seq).cuda(), torch.LongTensor(pos).cuda(), torch.LongTensor(neg).cuda()
+
+        pos_embed = self.item_embed(pos)
+        neg_embed = self.item_embed(neg)
+
+    def predict(self, seq, item_idxs):
+        pass
